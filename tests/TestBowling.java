9f10f74ff6530e9a78f7702b9dbc27d31549ce29
somename <someemail>
2017-06-09 20:27:53 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -64,10 +64,14 @@ public class TestBowling {
 	public void testGame4() {
 		
 		BowlingGame game=new BowlingGame();
-		Frame frame1=new Frame(10,0);
+		Frame frame1=new Frame(1,5); //6
 		game.addFrame(frame1);
-		game.setBonus(5, 2);
-		assertEquals(17,game.score());
+		Frame frame2=new Frame(2,8);	//10+1
+		game.addFrame(frame2);
+		Frame frame3=new Frame(1,7);	//8
+		game.addFrame(frame3);	
+		//14+11
+		assertEquals(25,game.score());
 		
 	}
 	
